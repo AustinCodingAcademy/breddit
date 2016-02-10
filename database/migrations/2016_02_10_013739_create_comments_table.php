8ec52b782a99032d07800a6dcc0d631000728296
Kevin Colten <kevin@austincodingacademy.com>
2016-02-09 19:54:46 -0600

    Added posts comments subbreddits join tables

---
+++ database/migrations/2016_02_10_013739_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('comment_id')->unsigned()->nullable();
+            $table->integer('post_id')->unsigned()->nullable();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
